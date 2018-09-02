d696c8f9ec4be41bab3a9ca3162a42b499a75155
Don Sanders <donsanders@Dons-MacBook-Pro.local>
2018-09-02 08:12:18 -0700

    App icon, and appearance frames for nodes

--- NetworkVisualizer/VisualizationView.swift
+++ NetworkVisualizer/VisualizationView.swift
@@ -270,7 +270,7 @@ class VisualizationView: UIButton {
             let p5 = CGPoint(x: 280, y: 585)
             positions = [p0, p1, p2, p3, p4, p5]
             velocities = [v0, v1, v2, v3, v3, v3]
-            appearance = [0, 0, 0, 0, 0, 0]
+            appearance = [0, 100, 200, 300, 400, 500]
             edges = [[0, 1, 1, 0, 0, 0],
                      [1, 0, 1, 0, 0, 0],
                      [1, 1, 0, 0, 0, 0],
